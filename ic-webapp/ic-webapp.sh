651a353fd498051894bd1ce9f356946c663a308c
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-07 14:40:03 +0200

    correction tag

--- ic-webapp/ic-webapp.sh
+++ ic-webapp/ic-webapp.sh
@@ -3,6 +3,6 @@ app="ic-webapp"
 port="8080"
 docker stop ${app} && docker rm ${app}
 docker build -t ${app}:1.0 .
-docker run -d -p ${port}:${port} --name=${app} ${app}
+docker run -d -p ${port}:${port} --name=${app} ${app}:1.0
 
 curl http://localhost:${port}
