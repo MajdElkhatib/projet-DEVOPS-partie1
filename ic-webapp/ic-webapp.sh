3a0a838a6da6add08cfd738ed14aeb7a4d491200
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-07 14:46:21 +0200

    Ajout variables environnement

--- ic-webapp/ic-webapp.sh
+++ ic-webapp/ic-webapp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-app="ic-webapp"
+image="ic-webapp"
+name="test-ic-webapp"
 port="8080"
-docker stop ${app} && docker rm ${app}
-docker build -t ${app}:1.0 .
-docker run -d -p ${port}:${port} --name=${app} ${app}:1.0
+docker stop ${name} && docker rm ${name}
+docker build -t ${image}:1.0 .
+docker run -d -p ${port}:${port} \
+  -e ODOO_URL='https://www.odoo.com/' \
+  -e PGADMIN_URL='https://www.pgadmin.org/' \
+  --name=${name} ${image}:1.0
 
-sleep 5
+sleep 3
 
 curl http://localhost:${port}
