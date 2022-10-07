6d65fa60ad44af9fbfbe26e54c853feddfde7c28
cgarcia31 <christophe.garcia31@gmail.com>
2022-10-07 21:53:37 +0200

    Change timeout script install_app

--- infrastructure/minikube/install_app.sh
+++ infrastructure/minikube/install_app.sh
@@ -6,6 +6,6 @@ su vagrant -c "git clone https://github.com/Romain-Revel/ajc-projet-final.git /h
 su vagrant -c "cd /home/vagrant/ajc-projet-final.git && git checkout dev && cd manifests && kubectl apply -f ."
 
 echo "Wait for the availability of all pods with the label env=prod"
-su vagrant -c "kubectl wait --for=condition=ready pod -l env=prod -n icgroup --timeout=180s"
+su vagrant -c "kubectl wait --for=condition=ready pod -l env=prod -n icgroup --timeout=120s"
 
 echo "You can access http://$(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'):31500 to use the ic-webapp"
