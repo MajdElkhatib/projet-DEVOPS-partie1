56d0ff8a88dadfab113b470b49e667dd73a364ab
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-07 15:04:06 +0200

    Ajout docker hub username

--- ic-webapp/ic-webapp.sh
+++ ic-webapp/ic-webapp.sh
@@ -3,11 +3,11 @@ image="ic-webapp"
 name="test-ic-webapp"
 port="8080"
 docker stop ${name} && docker rm ${name}
-docker build -t ${image}:1.0 .
+docker build -t sh0t1m3/${image}:1.0 .
 docker run -d -p ${port}:${port} \
   -e ODOO_URL='https://www.odoo.com/' \
   -e PGADMIN_URL='https://www.pgadmin.org/' \
-  --name=${name} ${image}:1.0
+  --name=${name} sh0t1m3/${image}:1.0
 
 sleep 3
 
