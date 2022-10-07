16e526526f266b659299d590fcb673a58a6b43f1
Romain REVEL <rrr.revel@hotmail.fr>
2022-10-07 15:04:19 +0200

    Ajout script nettoyage et publication

---
+++ ic-webapp/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+image="ic-webapp"
+name="test-ic-webapp"
+
+docker stop ${name}
+docker rm ${name}
+
+docker login
+docker push sh0t1m3/${image}:1.0
+
+# docker rmi ${image}
+# docker rm $(docker ps -aq)
+# docker rmi $(docker images -aq)
