f1af1bf51518cc5f019c848973278ab35ad1195b
Christophe Garcia <christophe.garcia31@gmail.com>
2022-10-11 07:57:57 +0200

    Modification du fichier install_app.sh infra

--- infrastructure/ic-webapp/install_app.sh
+++ infrastructure/ic-webapp/install_app.sh
@@ -3,9 +3,5 @@
 yum -y install git
 
 su vagrant -c "git clone https://github.com/Romain-Revel/ajc-projet-final.git /home/vagrant/ajc-projet-final"
-su vagrant -c "cd /home/vagrant/ajc-projet-final && git checkout dev && cd manifests && kubectl apply -f ."
-sleep 3
-echo "Wait for the availability of all pods with the label env=prod"
-su vagrant -c "kubectl wait --for=condition=ready pod -l env=prod -n icgroup --timeout=120s"
+su vagrant -c "cd /home/vagrant/ajc-projet-final && git checkout dev && cd manifests && sh install_app.sh -q"
 
-echo "You can access http://$(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'):31500 to use the ic-webapp"
